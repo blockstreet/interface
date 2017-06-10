bb725362b6b7925740eec9fde06c37e750eeedd4
Nick <nkmlombardi@gmail.com>
2017-06-09 20:04:54 -0400

    Can now change API url dynamically through .environment file.

--- configuration/dev.env.js
+++ configuration/dev.env.js
@@ -1,7 +1,9 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
+var env = require('node-env-file')
+env('.environment')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    API_LOCATION: '"http://staging.blockstreet.io/api"'
+    API_LOCATION: `"${process.env.DEV_API}"`
 })
--- configuration/prod.env.js
+++ configuration/prod.env.js
@@ -1,4 +1,7 @@
+var env = require('node-env-file')
+env('.environment')
+
 module.exports = {
     NODE_ENV: '"production"',
-    API_LOCATION: '"https://blockstreet.io/api"',
+    API_LOCATION: `"${process.env.PROD_API}"`
 }
